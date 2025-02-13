9d09e49a155f24b1d0c30a1874f031bf507e1d8c
John San Martin <jhonsanmartin11@gmail.com>
2025-02-12 21:11:08 -0500

    Add media backup and update build configuration

--- chocorocks/chocorocks/urls.py
+++ chocorocks/chocorocks/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Servir archivos media tanto en desarrollo como en producción
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
